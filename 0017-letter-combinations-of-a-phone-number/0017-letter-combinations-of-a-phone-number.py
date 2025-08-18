f247788733d353e2c7a75ec3bb99c9b4fce27342
amiz-seoat <150024337+amiz-seoat@users.noreply.github.com>
2025-08-18 03:47:29 -0400

    Time: 0 ms (100%), Space: 12.2 MB (99%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+
+        phone_map = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+
+        result = []
+
+        def backtrack(index, path):
+            # Base case: if path length == digits length
+            if index == len(digits):
+                result.append("".join(path))
+                return
+            
+            # Explore all possible letters for current digit
+            for letter in phone_map[digits[index]]:
+                path.append(letter)
+                backtrack(index + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return result
